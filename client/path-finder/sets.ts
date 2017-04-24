0ff62d2a305110eee27dcca081b2f8deb3e1d0a0
Zaggen <xpynesoul@gmail.com>
2017-04-23 21:36:51 -0500

    Removed a couple of issues with the tracker on the minSet

--- client/path-finder/sets.ts
+++ client/path-finder/sets.ts
@@ -1,4 +1,4 @@
-declare const _: any
+import * as _ from 'lodash'
 
 interface SetI<T> {
   add(item: T): void
@@ -6,18 +6,22 @@ interface SetI<T> {
   contains(item: T): boolean
   forEach(cb: Function)
   toArray(): T[]
+  length: number
 }
 
 export class MapSet<T> implements SetI<T> {
   _store: {[key: string]: T} = {}
+  _length = 0
   constructor(items?: T[]){
     items && items.forEach(this.add.bind(this))
   }
   add(item: T){
     this._store[item.toString()] = item
+    this._length++
   }
   remove(item: T){
     delete this._store[item.toString()]
+    this._length--
   }
   contains(item: T): boolean{
     return Boolean(this._store[item.toString()])
@@ -34,11 +38,18 @@ export class MapSet<T> implements SetI<T> {
     )
     return arr
   }
+  get length(){
+    return this._length
+  }
 }
 
+export interface ComparableItemI {
+  [key: string]: any
+  compareTo(b): -1 | 0 | 1
+  toString(): string
+}
 
-
-export class MinHeapSet<T extends {compareTo(b): -1 | 0 | 1}> implements SetI<T> {
+export class MinHeapSet<T extends ComparableItemI> implements SetI<T> {
   _flatTree: T[] = []
   _tracker: {[key: string]: number} = {}
   constructor(items?: T[]){
@@ -51,16 +62,19 @@ export class MinHeapSet<T extends {compareTo(b): -1 | 0 | 1}> implements SetI<T>
     if(this._flatTree.length > 1){
       const lastItem = this._flatTree.pop()
       this._flatTree[0] = lastItem
-      this._tracker[lastItem.toString()] = this._sortDown(0)
+      this._tracker[lastItem.toString()] = 0
+      this._sortDown(0)
     }
     else {
       this._flatTree = []
+      this._tracker = {}
     }
     return firstItem
   }
   add(item: T){
     this._flatTree.push(item)
-    this._tracker[item.toString()] = this._sortUp(this._flatTree.length - 1)
+    this._tracker[item.toString()] = this._flatTree.length - 1
+    this._sortUp(this._flatTree.length - 1)
   }
   get length(): number{
     return this._flatTree.length
@@ -68,7 +82,7 @@ export class MinHeapSet<T extends {compareTo(b): -1 | 0 | 1}> implements SetI<T>
   contains(item: T){
     return _.isNumber(this._tracker[item.toString()])
   }
-  getItem(itemKey){
+  getItemByKey(itemKey: string){
     const itemIndex = this._tracker[itemKey]
     return this._flatTree[itemIndex]
   }
@@ -78,21 +92,20 @@ export class MinHeapSet<T extends {compareTo(b): -1 | 0 | 1}> implements SetI<T>
   toArray(){
     return _.clone(this._flatTree)
   }
-  _sortUp(index: number): number{
-    let parentIndex = Math.floor((index - 1) / 2)
+  _sortUp(index: number): void {
     let currentIndex = index
     while(true){
+      const parentIndex = Math.floor((currentIndex - 1) / 2)
       const parentItem = this._flatTree[parentIndex]
       const currentItem = this._flatTree[currentIndex]
       if(currentIndex == 0)
-        return currentIndex
+        return
       else if(currentItem.compareTo(parentItem) < 0){
-        parentIndex = Math.floor((index - 1) / 2)
-        this._swap(index, parentIndex)
+        this._swap(currentIndex, parentIndex)
         currentIndex = parentIndex
       }
       else {
-        return currentIndex
+        return
       }
     }
   }
@@ -100,8 +113,15 @@ export class MinHeapSet<T extends {compareTo(b): -1 | 0 | 1}> implements SetI<T>
     const tempA = this._flatTree[aIndex]
     this._flatTree[aIndex] = this._flatTree[bIndex]
     this._flatTree[bIndex] = tempA
+    this._swapTrackerInfo(aIndex, bIndex)
   }
-  _sortDown(index: number): number{
+  _swapTrackerInfo(aIndex, bIndex){
+    const aItem = this._flatTree[aIndex]
+    const bItem = this._flatTree[aIndex]
+    this._tracker[aItem.toString()] = bIndex
+    this._tracker[bItem.toString()] = aIndex
+  }
+  _sortDown(index: number): void{
     let currentIndex = index
     while(true){
       const childIndexLeft = (currentIndex * 2) + 1
@@ -122,10 +142,10 @@ export class MinHeapSet<T extends {compareTo(b): -1 | 0 | 1}> implements SetI<T>
         }
         // Else the node is in the right position so we leave it there
         else
-          return currentIndex
+          return
       }
       else
-        return currentIndex
+        return
     }
   }
   _getItem(index: number): T {
