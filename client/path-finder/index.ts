95b7a33b62ec319c95ea9dc13fa0ec142d1e63cc
Zaggen <xpynesoul@gmail.com>
2017-04-24 09:20:12 -0500

    Some refactoring. We now get the rowsQ and colsQ from the passed tileMap

--- client/path-finder/index.ts
+++ client/path-finder/index.ts
@@ -2,36 +2,35 @@ import * as _ from 'lodash'
 import {MapSet, MinHeapSet} from './sets'
 import {PathFindingNode} from './node.class'
 
-type Tile = [number, number]
+export type Tile = [number, number]
 // TODO: Get rowsQ and colsQ from the passed tileMap
-const rowsQ = 10
-const colsQ = 10
+//const rowsQ = 10
+//const colsQ = 10
 
 // TODO: Implement obstacles with a second layer of tiles
 // TODO: Fix issues with some paths
-export function pathFinder(tileMap, startTile: Tile, endTile: Tile, callback){
+export function pathFinder(tileMap: any[][], startTile: Tile, endTile: Tile, callback?){
   const closedSet = new MapSet<PathFindingNode>()
   const openSet = new MinHeapSet<PathFindingNode>()
+  const rowsQ = tileMap.length
+  const colsQ = tileMap[0].length
   const NEXT_TILE_MOVEMENT_COST = 1
   // const startTileK = JSON.stringify(startTile)
   openSet.add(new PathFindingNode(startTile, 0, getHeuristicDistance(startTile, endTile)))
   while(openSet.length > 0){
     const currentNode = openSet.removeMin()
-    callback(openSet, closedSet, reconstructPath(currentNode))
+    callback && callback(openSet, closedSet, reconstructPath(currentNode))
     closedSet.add(currentNode)
     if(currentNode.val[0] == endTile[0] && currentNode.val[1] == endTile[1]){
-      const path = reconstructPath(currentNode)
-      debugger
-      console.log('Found path', path)
-      return path
+      return reconstructPath(currentNode)
     }
 
-    const neighborTiles = getTileNeighbors(currentNode.val)
+    const neighborTiles = getTileNeighbors(currentNode.val, rowsQ, colsQ)
     neighborTiles.forEach((tile: Tile)=> {
       if(!closedSet.contains(<any>tile)){
         const tempG = currentNode.g + NEXT_TILE_MOVEMENT_COST
         if(openSet.contains(<any>tile)){
-          const neighborNode = <PathFindingNode>openSet.getItem(tile)
+          const neighborNode = <PathFindingNode>openSet.getItemByKey(tile.toString())
           if(tempG < neighborNode.g){
             neighborNode.g = tempG
             neighborNode.previousNode = currentNode
@@ -51,18 +50,16 @@ function reconstructPath(node: PathFindingNode){
     invertedPath.push(node.previousNode.val)
     node = node.previousNode
   }
-  const path = invertedPath.reverse()
-  debugger
-  return path
+  return invertedPath.reverse()
 }
 
 function getHeuristicDistance(a: Tile, b: Tile){
-  const horizontalDistance = Math.abs(a[0]) - Math.abs(b[0])
-  const verticalDistance = Math.abs(a[1]) - Math.abs(b[1])
-  return Math.abs(horizontalDistance) + Math.abs(verticalDistance)
+  const horizontalDistance = Math.abs(a[0] - b[0])
+  const verticalDistance = Math.abs(a[1] - b[1])
+  return horizontalDistance + verticalDistance
 }
 
-function getTileNeighbors(tile: Tile): Tile[]{
+function getTileNeighbors(tile: Tile, rowsQ: number, colsQ: number): Tile[]{
   const [row, col] = tile
   const up    = (row - 1 >= 0) ? [row - 1, col] : null
   const left  = (col - 1 >= 0) ? [row, col - 1] : null
